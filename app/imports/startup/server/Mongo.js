e120d700123af937c5a922bf9d266a319fdd57b6
Chak Hon <chakhon@hawaii.edu>
2021-10-19 01:47:38 -1000

    tags

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Tag } from '../../api/Event/Tag';
 
 /* eslint-disable no-console */
 
@@ -9,10 +10,22 @@ function addData(data) {
   Stuffs.collection.insert(data);
 }
 
+function addTag(tag) {
+  console.log(`  Adding: ${tag.tag} `);
+  Tag.collection.insert(tag);
+}
+
 // Initialize the StuffsCollection if empty.
 if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
+  }
+}
+
+if (Tag.collection.find().count() === 0) {
+  if (Meteor.settings.defaultData) {
+    console.log('Creating default tags.');
+    Meteor.settings.defaultHobbies.map(tag => addTag(tag));
   }
 }
